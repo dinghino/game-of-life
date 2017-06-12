5018bc995bb59717f27d97c28bf1da3a81f740b5
Daniele Ding <dinghino@gmail.com>
2017-06-12 19:18:06 +0200

    imports cleanup
    avoid confusion

--- game/__init__.py
+++ game/__init__.py
@@ -1,4 +1,2 @@
 import game.states as states            # noqa
 import game.game as game                # noqa
-from .game import generate              # noqa
-from .states import generate_from_file  # noqa
